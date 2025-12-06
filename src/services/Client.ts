a8e6813705ae7025e44c18f92a2e781b301f3700
din <surabordin@windowslive.com>
2025-12-06 21:48:09 +0700

    feat: add service layer

--- src/services/Client.ts
+++ src/services/Client.ts
@@ -1,45 +1,68 @@
-import { ApiResult } from "../api/ApiResult";
+import Service from "./Service";
 import TransactionInput from "../models/TransactionInput";
+import { ApiResult } from "../api/ApiResult";
+import Status from "../models/Status";
+import sleep from "../api/Sleep";
+import TransactionStatus from "../models/TransactionStatus";
 
-class Client {
-  private defaultOptions: RequestInit = {
-    headers: { "Content-Type": "application/json" },
-  };
+export default class Client {
+  private service: Service;
 
   constructor() {
-    this.debug("Client initialized");
+    this.service = new Service();
   }
 
-  post = async <T>(
+  broadcastTransaction = async <TransactionHash>(
     url: string,
-    body: TransactionInput
-  ): Promise<ApiResult<T>> => {
-    if (!url) {
-      return { data: null, error: "Missing URL" };
+    payload: TransactionInput
+  ): Promise<ApiResult<TransactionHash>> => {
+    const data = await this.service.post<TransactionHash>(url, payload);
+    if (data.error) {
+      console.error("Failed to broadcast transaction :", data.error);
+    } else {
+      console.log("Your transaction hash :", data.data);
     }
-    const options: RequestInit = {
-      ...this.defaultOptions,
-      method: "POST",
-      body: JSON.stringify(body),
-    };
-    return this.handleRequest<T>(() => fetch(url, options));
+    return data;
   };
 
-  private handleRequest = async <T>(
-    request: () => Promise<Response>
-  ): Promise<ApiResult<T>> => {
-    const res = await request();
+  transactionStatusMonitoring = async (
+    tx_hash: string
+  ): Promise<ApiResult<TransactionStatus>> => {
+    let DEFAULT_RETRY_REQUEST: number = 10;
+    let status = await this.service.get<TransactionStatus>(tx_hash);
 
-    if (!res.ok) {
-      const errorText = await res.text();
-      return { data: null, error: errorText };
+    while (
+      status.data?.tx_status === Status.PENDING &&
+      DEFAULT_RETRY_REQUEST > 0
+    ) {
+      status = await this.service.get<TransactionStatus>(tx_hash);
+
+      if (status.error) {
+        console.error("[Error] :", status.error);
+      } else if (status.data?.tx_status != null) {
+        this.status(status.data?.tx_status);
+      } else {
+        this.status("null");
+      }
+      if (status.data?.tx_status === Status.CONFIRMED) {
+        this.status("Transaction has been confirmed");
+        break;
+      }
+      if (status.data?.tx_status === Status.FAILED) {
+        this.status("Transaction has been failed, please try again");
+        break;
+      }
+      if (DEFAULT_RETRY_REQUEST == 0) {
+        this.status("Time out");
+        break;
+      }
+      await sleep(5000);
+      DEFAULT_RETRY_REQUEST--;
     }
-    return { data: await (res.json() as Promise<T>), error: null };
+    return status;
   };
 
-  private debug = (message: string) => {
-    console.log(`[DEBUG] ${message}`);
+  private status = (message: string) => {
+    console.log(`[STATUS] ${message}`);
   };
 }
-
-export default Client;
---
+++ src/services/Service.ts
@@ -0,0 +1,59 @@
+import { ApiResult } from "../api/ApiResult";
+import TransactionInput from "../models/TransactionInput";
+
+export default class Service {
+  private defaultOptions: RequestInit = {
+    headers: { "Content-Type": "application/json" },
+  };
+
+  constructor() {
+    this.debug("Client initialized");
+  }
+
+  get = async <T>(transaction_hash: string): Promise<ApiResult<T>> => {
+    if (!transaction_hash) {
+      return { data: null, error: "Incorrect transaction hash" };
+    }
+    const options: RequestInit = {
+      ...this.defaultOptions,
+      method: "GET",
+    };
+    return this.handleRequest<T>(() =>
+      fetch(
+        `https://mock-node-wgqbnxruha-as.a.run.app/check/${transaction_hash}`,
+        options
+      )
+    );
+  };
+
+  post = async <T>(
+    url: string,
+    body: TransactionInput
+  ): Promise<ApiResult<T>> => {
+    if (!url) {
+      return { data: null, error: "Missing URL" };
+    }
+    const options: RequestInit = {
+      ...this.defaultOptions,
+      method: "POST",
+      body: JSON.stringify(body),
+    };
+    return this.handleRequest<T>(() => fetch(url, options));
+  };
+
+  private handleRequest = async <T>(
+    request: () => Promise<Response>
+  ): Promise<ApiResult<T>> => {
+    const res = await request();
+
+    if (!res.ok) {
+      const errorText = await res.text();
+      return { data: null, error: errorText };
+    }
+    return { data: await (res.json() as Promise<T>), error: null };
+  };
+
+  private debug = (message: string) => {
+    console.log(`[DEBUG] ${message}`);
+  };
+}
